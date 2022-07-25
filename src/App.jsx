39b22582b08562b92b7475c32361201e77e09714
Dmytro Zhyltsov <tomatguitar@gmail.com>
2022-07-26 01:06:55 +0300

    Added GoogleSingIn auth method

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import Home from './routes/Home/Home';
 import Navigation from './routes/Navigation/Navigation';
+import SignIn from './routes/SignIn/SignIn';
 
 const Shop = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
